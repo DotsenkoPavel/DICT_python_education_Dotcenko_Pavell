5ae062de470f24040ed5e8c83626bae423b17af7
DotsenkoPavel <p.p.dotsenko@student.khai.edu>
2021-10-04 08:40:33 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -3,3 +3,6 @@ Birth_year = "2021"
 print("Hello! My name is" + " " + bot_name)
 print("I was created in" + " " + Birth_year)
 
+print("Please, remind me your name.")
+your_name = str(input())
+print("What a great name you have, " + your_name + "!")
